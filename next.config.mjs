2c2b72774282d2c1f97efdcfb8e84062f37a2c48
SupawitKaennak <Supawitkaennak@hotmail.com>
2026-07-11 19:56:08 +0700

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const repo = 'MyPortfolio';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
-  output: 'export',  
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   eslint: {
     ignoreDuringBuilds: true,
   },
